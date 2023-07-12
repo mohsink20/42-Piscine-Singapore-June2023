abb6e587697bf380e5079bb56679c0d38baf99b1
shu <sshuytbusiness@gmail.com>
2023-07-12 17:14:13 +0700

    Update

--- C10/ex01/main.c
+++ C10/ex01/main.c
@@ -1,37 +1,67 @@
-#include <unistd.h>
 #include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <libgen.h>
+#include "cat.h"
 
-void	ft_putstr(char *str)
+#define BUF_SIZE 1
+
+void print_string(char *str)
 {
-	while (*str)
-		write(1, str++, 1);
+    while (*str)
+        write(1, str++, 1);
 }
 
-int		main(int argc, char *argv[])
+void print_error_message(ProgramInfo *info, char *file)
 {
-	char	c;
-	int		a;
-	int		b;
+    print_string(basename(info->program_name));
+    print_string(": ");
+    print_string(file);
+    print_string(": ");
+    print_string(strerror(errno));
+    print_string("\n");
+    errno = 0;
+}
 
-	if (argc == 1)
-		ft_putstr("File name missing.\n");
-	else if (argc > 2)
-		ft_putstr("Too many arguments.\n");
-	else
-	{
-		b = open(argv[1], O_RDONLY);
-		if (b == -1)
-			ft_putstr("Cannot read file.\n");
-		else
-		{
-			a = read(b, &c, 1);
-			while (a)
-			{
-				write(1, &c, 1);
-				a = read(b, &c, 1);
-			}
-			close(b);
-		}
-	}
-	return (0);
+void display_file_contents(int fd, ProgramInfo *info, char *file)
+{
+    long size;
+    unsigned char buf[BUF_SIZE];
+
+    while ((size = read(fd, buf, BUF_SIZE)))
+    {
+        if (errno)
+        {
+            print_error_message(info, file);
+            return;
+        }
+        write(1, buf, size);
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    ProgramInfo info;
+    int fd;
+    int i;
+
+    info.program_name = argv[0];
+    if (argc == 1)
+        display_file_contents(0, &info, 0);
+    else
+    {
+        i = 0;
+        while (++i < argc)
+        {
+            if ((fd = open(argv[i], O_RDONLY)) == -1)
+            {
+                print_error_message(&info, argv[i]);
+                continue;
+            }
+            display_file_contents(fd, &info, argv[i]);
+            close(fd);
+        }
+    }
+    return 0;
 }
